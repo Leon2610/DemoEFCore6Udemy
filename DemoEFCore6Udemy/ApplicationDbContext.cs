f1a8cc5e3c66b11f25a58a2306c4542032cc8eb6
Leonel Leon <leonleo1996@gmail.com>
2023-05-04 23:20:17 -0600

    Initial commit

--- DemoEFCore6Udemy/ApplicationDbContext.cs
+++ DemoEFCore6Udemy/ApplicationDbContext.cs
@@ -9,5 +9,11 @@ namespace DemoEFCore6Udemy
 {
     public class ApplicationDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MORDOR\\LEODATABASE;Database=DemoEFCore6Udemy;Integrated Security=True;TrustServerCertificate=Yes");
+        }
+
+        public DbSet<Persona> Personas { get; set; }
     }
 }
